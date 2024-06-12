c65aafa590f8157be3fc35d7a10b9022b36b239a
Jake Loew <jake@madebychalk.com>
2024-06-12 11:06:00 -0600

    Add SDK

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -3,36 +3,32 @@
 import { PropsWithChildren } from "react";
 
 import {
-  FusionAuthProvider,
-  FusionAuthProviderConfig,
+	FusionAuthProvider,
+	FusionAuthProviderConfig,
 } from "@fusionauth/react-sdk";
 import { useCookies } from "next-client-cookies";
 
 const config: FusionAuthProviderConfig = {
-  clientId: "e9fdb985-9173-4e01-9d73-ac2d60d1dc8e",
-  redirectUri: "http://localhost:3000",
-  serverUrl: "http://localhost:9011",
-  shouldAutoFetchUserInfo: true,
-  shouldAutoRefresh: true,
-  onRedirect: (state?: string) => {
-    console.log(`Redirect happened with state value: ${state}`);
-  },
-  scope: "openid email profile offline_access",
+	clientId: "e9fdb985-9173-4e01-9d73-ac2d60d1dc8e",
+	redirectUri: "http://localhost:3000",
+	serverUrl: "http://localhost:9011",
+	shouldAutoFetchUserInfo: true,
+	shouldAutoRefresh: true,
+	autoRefreshSecondsBeforeExpiry: 60 * 60 - 5,
+	onRedirect: (state?: string) => {
+		console.log(`Redirect happened with state value: ${state}`);
+	},
+	scope: "openid email profile offline_access",
+	nextCookieAdapter: useCookies,
+	onAutoRefreshFailure(e) {
+		console.error(e);
+	},
 };
 
 export default function Providers({ children }: PropsWithChildren) {
-  const cookies = useCookies();
-
-  const cookieAdapter = {
-    at_exp() {
-      return cookies.get("app.at_exp");
-    },
-  };
-
-  const c = {
-    ...config,
-    cookieAdapter,
-  };
-
-  return <FusionAuthProvider {...c}>{children}</FusionAuthProvider>;
+	return (
+		<FusionAuthProvider<{ name: string }> {...config}>
+			{children}
+		</FusionAuthProvider>
+	);
 }
